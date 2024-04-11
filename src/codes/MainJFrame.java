c7285ef77e0b351c4a4523de4737684d4c8bddc5
Gaël COUGNARD <gael.cougnard@edu.ece.fr>
2024-04-11 19:57:12 +0200

    Meilleur affichage + début page de connexion

--- src/codes/MainJFrame.java
+++ src/codes/MainJFrame.java
@@ -7,7 +7,7 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.Serial;
 
-public class MainJFrame extends JFrame implements WindowListener, ComponentListener {
+public class MainJFrame extends JFrame implements WindowListener, ComponentListener, MouseListener, ActionListener {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -15,16 +15,31 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
 
     JFrame frame = new JFrame("Window");
 
-    private final GridBagLayout gridBagLayout = new GridBagLayout();
-    private final GridBagConstraints constraints = new GridBagConstraints();
+    private final GridBagLayout mainGridBagLayoutConnectionIscriptionMenu = new GridBagLayout();
+    private final GridBagLayout innerGridBagLayoutConnectionInscription = new GridBagLayout();
+    private final GridBagLayout mainGridBagLayoutConnectionMenu = new GridBagLayout();
+    private final GridBagLayout innerGridBagLayoutConnection = new GridBagLayout();
+    private final BorderLayout borderLayout = new BorderLayout();
+    private final GridBagConstraints constraintsMainConnecInsc = new GridBagConstraints();
+    private final GridBagConstraints constraintsInnerConnectionInscription = new GridBagConstraints();
+    private final GridBagConstraints constraintsMainConnec = new GridBagConstraints();
+    private final GridBagConstraints constraintsInnerConnec = new GridBagConstraints();
+
+
+    private JPanel panelContainer = new JPanel();
+    private JPanel inscrConnecMainPanel = new JPanel();
+    private JPanel inscrMainPanel = new JPanel();
+    private JPanel connecMainPanel = new JPanel();
 
 
     private JLabel legendaryMotorsportLabel = new JLabel("LEGENDARY MOTORSPORT");
+    private JLabel legendaryMotorsportLabel2 = new JLabel("LEGENDARY MOTORSPORT");
 
-    private JPanel mainLabel = new JPanel();
     private JPanel legendaryMotorsportPanel = new JPanel();
+    private JPanel legendaryMotorsportPanel2 = new JPanel();
     private JPanel connexionPanel = new JPanel();
     private JPanel inscriptionPanel = new JPanel();
+    private JPanel connexionInscriptionBigPanel = new JPanel();
 
     private JButton buttonConnexion = new JButton("Connexion");
     private JButton buttonInscription = new JButton("Inscription");
@@ -35,6 +50,37 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
     private Dimension dimensionConnexionPanel = new Dimension(windowSizeWidth/8, windowSizeHeight/10);
     private Dimension dimensionInscriptionPanel = new Dimension(windowSizeWidth/8, windowSizeHeight/10);
 
+    private int fontSizeLM = 36;
+    private int fontSizeConnexion = 24;
+    private int getFontSizeInscription = 24;
+    private Font font1 = new Font("Arial", Font.PLAIN, fontSizeLM);
+    private Font font2 = new Font("Arial", Font.PLAIN, fontSizeConnexion);
+    private Font font3 = new Font("Arial", Font.PLAIN, getFontSizeInscription);
+
+
+
+
+
+    private JPanel usernamePWPanel = new JPanel();
+    private JLabel backToMainMenu = new JLabel("RETOUR");
+    private JLabel sInscrire = new JLabel("S'inscrire !");
+    private JPanel topPanelConnec = new JPanel();
+
+    private JLabel displayUsernameField = new JLabel("Adresse E-mail : ");
+    private JLabel displayPasswordField = new JLabel("Mot de passe : ");
+
+    private JTextField usernameField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+
+    private JButton connectionButton = new JButton("Connection");
+
+
+
+
+
+
+    CardLayout cardLayout = new CardLayout();
+
     public MainJFrame(){
         super("Client");
         frame.addWindowListener(this);
@@ -44,49 +90,172 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
         this.windowSizeWidth = GlobalVariable.getScreenWidth();
         this.windowSizeHeight = GlobalVariable.getScreenHeight();
 
-        this.mainLabel.setLayout(gridBagLayout);
-        this.mainLabel.setBackground(Color.WHITE);
+
+        panelContainer.setLayout(cardLayout);
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////// PREMIERE PAGE A ETRE AFFICHEE : CONNEXION / INSCRIPTION  ///////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        this.inscrConnecMainPanel.setLayout(mainGridBagLayoutConnectionIscriptionMenu);
+        this.inscrConnecMainPanel.setBackground(Color.WHITE);
+        this.connexionInscriptionBigPanel.setLayout(innerGridBagLayoutConnectionInscription);
 
         this.legendaryMotorsportLabel.setOpaque(true); // Permet de définir la couleur de fond
-        this.legendaryMotorsportLabel.setBackground(Color.BLUE); // Couleur de fond du carré
-        this.legendaryMotorsportLabel.setForeground(Color.WHITE); // Couleur du texte
+        this.legendaryMotorsportLabel.setForeground(Color.MAGENTA); // Couleur du texte
         this.buttonConnexion.setOpaque(true); // Permet de définir la couleur de fond
-        this.buttonConnexion.setBackground(Color.BLUE); // Couleur de fond du carré
-        this.buttonConnexion.setForeground(Color.WHITE); // Couleur du texte
+        this.buttonConnexion.setForeground(Color.MAGENTA); // Couleur du texte
         this.buttonInscription.setOpaque(true); // Permet de définir la couleur de fond
-        this.buttonInscription.setBackground(Color.BLUE); // Couleur de fond du carré
-        this.buttonInscription.setForeground(Color.WHITE); // Couleur du texte
+        this.buttonInscription.setForeground(Color.MAGENTA); // Couleur du texte
 
-        Font font = new Font("Arial", Font.PLAIN, 26);
 
         // Appliquer la police au JLabel
-        legendaryMotorsportLabel.setFont(font);
-        buttonConnexion.setFont(font);
-        buttonInscription.setFont(font);
+        legendaryMotorsportLabel.setFont(font1);
+        buttonConnexion.setFont(font2);
+        buttonInscription.setFont(font3);
 
         legendaryMotorsportPanel.add(this.legendaryMotorsportLabel);
+
+
+        buttonConnexion.setActionCommand("CONNECTION");
+        buttonConnexion.addActionListener(this);
         connexionPanel.add(this.buttonConnexion);
+
+        buttonInscription.setActionCommand("INSCRIPTION");
+        buttonInscription.addActionListener(this);
         inscriptionPanel.add(this.buttonInscription);
 
-        this.constraints.gridx=0;
-        this.constraints.gridy=0;
-        this.constraints.anchor = GridBagConstraints.CENTER;
-        this.constraints.weightx = 1.0;
-        this.constraints.weighty = 1.0;
+        this.constraintsMainConnecInsc.gridx=0;
+        this.constraintsMainConnecInsc.gridy=0;
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.CENTER;
+        this.constraintsMainConnecInsc.weightx = 1.0;
+        this.constraintsMainConnecInsc.weighty = 1.5;
 
         legendaryMotorsportPanel.setPreferredSize(dimensionLegendaryMotorsportPanel);
         connexionPanel.setPreferredSize(dimensionConnexionPanel);
         inscriptionPanel.setPreferredSize(dimensionInscriptionPanel);
-
-        mainLabel.add(this.legendaryMotorsportPanel, this.constraints);
-        this.constraints.gridy=1;
-        mainLabel.add(connexionPanel, this.constraints);
-        this.constraints.gridy=2;
-        mainLabel.add(inscriptionPanel, this.constraints);
+        inscrConnecMainPanel.add(this.legendaryMotorsportPanel, this.constraintsMainConnecInsc);
 
 
-        frame.add(mainLabel);
+        this.constraintsInnerConnectionInscription.gridx=0;
+        this.constraintsInnerConnectionInscription.gridy=0;
+        this.constraintsInnerConnectionInscription.anchor = GridBagConstraints.CENTER;
+        this.constraintsInnerConnectionInscription.weightx = 1.0;
+        this.constraintsInnerConnectionInscription.weighty = 1.0;
+        this.connexionInscriptionBigPanel.add(this.connexionPanel, this.constraintsInnerConnectionInscription);
+        this.constraintsInnerConnectionInscription.gridy=1;
+        this.connexionInscriptionBigPanel.add(this.inscriptionPanel, this.constraintsInnerConnectionInscription);
 
+        this.constraintsMainConnecInsc.gridy=1;
+        this.constraintsMainConnecInsc.weighty = 4.5;
+
+        inscrConnecMainPanel.add(this.connexionInscriptionBigPanel, this.constraintsMainConnecInsc);
+
+
+        panelContainer.add(inscrConnecMainPanel);
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////// SECONDE PAGE A ETRE AFFICHEE : CONNEXION  //////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Initialisation :
+        this.connecMainPanel.setLayout(mainGridBagLayoutConnectionIscriptionMenu);
+        this.usernamePWPanel.setLayout(mainGridBagLayoutConnectionMenu);
+        this.topPanelConnec.setLayout(innerGridBagLayoutConnection);
+        this.connecMainPanel.setBackground(Color.WHITE);
+
+        // Initialisation des contraintes :
+        this.constraintsMainConnecInsc.gridx=0;
+        this.constraintsMainConnecInsc.gridy=0;
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.EAST;
+        this.constraintsMainConnecInsc.weightx = 1.0;
+        this.constraintsMainConnecInsc.weighty = 1.0;
+
+        // On met les différents éléments dans le panel :
+        this.usernamePWPanel.add(this.displayUsernameField, this.constraintsMainConnecInsc);
+        this.constraintsMainConnecInsc.gridx = 1;
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.WEST;
+        this.usernameField.setColumns(20);
+        this.usernamePWPanel.add(this.usernameField, this.constraintsMainConnecInsc);
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.EAST;
+        this.constraintsMainConnecInsc.gridx = 0;
+        this.constraintsMainConnecInsc.gridy = 1;
+        this.usernamePWPanel.add(this.displayPasswordField, this.constraintsMainConnecInsc);
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.WEST;
+        this.constraintsMainConnecInsc.gridx = 1;
+        this.passwordField.setColumns(20);
+        this.usernamePWPanel.add(this.passwordField, this.constraintsMainConnecInsc);
+        this.constraintsMainConnecInsc.gridx = 1;
+        this.constraintsMainConnecInsc.gridy = 2;
+        this.constraintsMainConnecInsc.anchor = GridBagConstraints.WEST;
+        this.usernamePWPanel.add(this.sInscrire, this.constraintsMainConnecInsc);
+
+
+
+
+        this.legendaryMotorsportLabel2.setOpaque(true); // Permet de définir la couleur de fond
+        this.legendaryMotorsportLabel2.setForeground(Color.MAGENTA); // Couleur du texte
+        this.legendaryMotorsportLabel2.setFont(font1);
+        this.legendaryMotorsportPanel2.add(this.legendaryMotorsportLabel2);
+        this.legendaryMotorsportPanel2.setPreferredSize(dimensionLegendaryMotorsportPanel);
+
+
+        this.constraintsInnerConnec.gridx = 0;
+        this.constraintsInnerConnec.gridy = 0;
+        this.constraintsInnerConnec.anchor = GridBagConstraints.NORTHWEST;
+
+
+
+        this.topPanelConnec.add(backToMainMenu, constraintsInnerConnec);
+        this.constraintsInnerConnec.gridy = 1;
+        this.constraintsInnerConnec.anchor = GridBagConstraints.CENTER;
+        this.topPanelConnec.add(legendaryMotorsportPanel2, constraintsInnerConnec);
+
+        // ajout des différents composants dans le panel principal.
+        this.constraintsMainConnec.gridx = 0;
+        this.constraintsMainConnec.gridy = 0;
+        this.constraintsMainConnec.anchor = GridBagConstraints.CENTER;
+        this.constraintsMainConnec.weightx = 1.0;
+        this.constraintsMainConnec.weighty = 1.5;
+        this.constraintsMainConnec.fill = GridBagConstraints.BOTH;
+
+        this.connecMainPanel.add(this.topPanelConnec, constraintsMainConnec);
+        this.constraintsMainConnec.anchor = GridBagConstraints.CENTER;
+        this.constraintsMainConnec.gridy = 1;
+        this.constraintsMainConnec.weighty = 4.5;
+        this.connecMainPanel.add(this.usernamePWPanel, constraintsMainConnec);
+
+
+
+
+
+
+
+
+
+        panelContainer.add(inscrMainPanel);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////// Ajout de toutes les pages au cardLayout  ///////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////// LIGNES FINALES  ///////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        panelContainer.add(inscrConnecMainPanel, "INSCRIPTION/CONNECTION");
+        panelContainer.add(connecMainPanel, "CONNECTION");
+        panelContainer.add(inscrMainPanel, "INSCRIPTION");
+        cardLayout.show(panelContainer, "INSCRIPTION/CONNECTION");
+
+        frame.add(panelContainer);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
     }
 
@@ -129,6 +298,18 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
 
     @Override
     public void componentResized(ComponentEvent e) {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////AJOUTER LA MOFIFICATION DE LA TAILLE DE LA POLICE SI ON A LE TEMPS ET LA DETER//////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //this.legendaryMotorsportLabel = new JLabel("LEGENDARY MOTORSPORT");
+        this.legendaryMotorsportPanel.add(this.legendaryMotorsportLabel);
+
+
+
         this.windowSizeHeight = this.frame.getSize().height;
         this.windowSizeWidth = this.frame.getSize().width;
         this.dimensionLegendaryMotorsportPanel = new Dimension(this.windowSizeWidth / 3, this.windowSizeHeight / 10);
@@ -138,7 +319,7 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
         this.legendaryMotorsportPanel.setPreferredSize(this.dimensionLegendaryMotorsportPanel);
         this.connexionPanel.setPreferredSize(this.dimensionConnexionPanel);
         this.inscriptionPanel.setPreferredSize(this.dimensionInscriptionPanel);
-        this.mainLabel.revalidate();
+        this.inscrConnecMainPanel.revalidate();
         this.repaint();
     }
 
@@ -155,5 +336,41 @@ public class MainJFrame extends JFrame implements WindowListener, ComponentListe
     @Override
     public void componentHidden(ComponentEvent e) {
 
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "INSCRIPTION/CONNECTION" -> cardLayout.show(panelContainer, "INSCRIPTION/CONNECTION");
+            case "CONNECTION" -> cardLayout.show(panelContainer, "CONNECTION");
+            case "INSCRIPTION" -> cardLayout.show(panelContainer, "INSCRIPTION");
+            case "page4" -> cardLayout.show(panelContainer, "page4");
+        }
     }
 }
