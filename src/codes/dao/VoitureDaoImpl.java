26389ffc6ccc7d9839c6568097245ded3aebcd69
Clement <clement.herbin@edu.ece.fr>
2024-05-17 22:36:05 +0200

    ajout fonction qui récupère la description d'un modèle grâce à l'id + ajout fonction qui permet de mettre à jour le prix d'une voiture + mise à jour des fonctions pour eviter les problèmes de connexion avec la bdd + ajout des services de paiement des réservations

--- src/codes/dao/VoitureDaoImpl.java
+++ src/codes/dao/VoitureDaoImpl.java
@@ -15,10 +15,20 @@ public class VoitureDaoImpl implements VoitureDao {
 
     @Override
     public void addVoiture(Voiture voiture) throws SQLException {
-        try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO voiture (prixParJour, id_modele) VALUES (?, ?)")) {
+
+        PreparedStatement preparedStatement;
+
+        try {
+
+            String query = "INSERT INTO voiture (prixParJour, id_modele) VALUES (?, ?)";
+            preparedStatement = connection.prepareStatement(query);
+
             preparedStatement.setFloat(1, voiture.getPrix_par_jour());
             preparedStatement.setInt(2, voiture.getId_modele());
             preparedStatement.execute();
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
     }
 
@@ -26,7 +36,13 @@ public class VoitureDaoImpl implements VoitureDao {
     public Voiture getVoiture(int id_voiture) throws SQLException {
         Voiture voiture = null;
 
-        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM voiture WHERE id=?")){
+        PreparedStatement statement;
+
+        try {
+
+            String query = "SELECT * FROM voiture WHERE id=?";
+            statement = connection.prepareStatement(query);
+
             statement.setInt(1, id_voiture);
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()){
@@ -36,30 +52,75 @@ public class VoitureDaoImpl implements VoitureDao {
                     voiture.setId_modele(resultSet.getInt("id_modele"));
                 }
             }
+        } catch (SQLException e){
+            e.printStackTrace();
         }
         return voiture;
     }
 
     @Override
     public int getIdVoiture(Voiture voiture) throws SQLException {
-        String query = "SELECT id FROM voiture WHERE statut = 0 AND id_modele = ? ORDER BY id LIMIT 1";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+        int idVoiture = -1;
+        PreparedStatement preparedStatement;
+
+        try {
+
+            String query = "SELECT id FROM voiture WHERE statut = 0 AND id_modele = ? ORDER BY id LIMIT 1";
+            preparedStatement = connection.prepareStatement(query);
+
             preparedStatement.setInt(1, voiture.getId_modele());
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
-                    return resultSet.getInt("id");
+                    idVoiture = resultSet.getInt("id");
                 } else {
                     throw new SQLException("No car found with statut = 0 and id_modele = " + voiture.getId_modele());
                 }
             }
+        } catch (SQLException e){
+            e.printStackTrace();
         }
+
+        return idVoiture;
     }
 
     @Override
     public void modifVoiture(Voiture voiture) throws SQLException{
-        try(PreparedStatement preparedStatement = connection.prepareStatement("UPDATE voiture SET statut = 1 WHERE id = ?")){
+
+        PreparedStatement preparedStatement;
+
+        try {
+
+            String query = "UPDATE voiture SET statut = 1 WHERE id = ?";
+
+            preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, voiture.getId_voiture());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
     }
+
+    @Override
+    public void updatePrixVoiture (int idVoiture, float nouveauPrix) throws SQLException {
+
+        PreparedStatement statement;
+
+        try {
+
+            String query = "UPDATE voiture SET prixParJour = ? WHERE id = ?";
+
+            statement = connection.prepareStatement(query);
+            statement.setFloat(1, nouveauPrix);
+            statement.setInt(2, idVoiture);
+            statement.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
