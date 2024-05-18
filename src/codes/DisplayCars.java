2cc5de017e150ab94dc42fab53eef7693a0c2e45
Clement <clement.herbin@edu.ece.fr>
2024-05-18 16:21:01 +0200

    test pour filter les modèles

--- src/codes/DisplayCars.java
+++ src/codes/DisplayCars.java
@@ -1,6 +1,11 @@
 package codes;
 
 import codes.dao.Mysql;
+import codes.dao.Type_voitureDaoImpl;
+import codes.model.Client;
+import codes.model.Type_voiture;
+import codes.model.Voiture;
+import com.mysql.cj.conf.ConnectionUrlParser;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -13,6 +18,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class DisplayCars {
 
@@ -73,22 +80,307 @@ public class DisplayCars {
                 }
                 break;
             case "BMW":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String marque = "bmw";
+                    List<Type_voiture> modeles = modeleDao.searchModele(marque);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "AUDI":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String marque = "audi";
+                    List<Type_voiture> modeles = modeleDao.searchModele(marque);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Prix croissant":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    List<ConnectionUrlParser.Pair<Type_voiture, Voiture>> modeleVoiturePairs = modeleDao.searchAllModeleASCPrice();
+
+                    if (!modeleVoiturePairs.isEmpty()) {
+                        for (ConnectionUrlParser.Pair<Type_voiture, Voiture> modeleVoiturePair : modeleVoiturePairs) {
+                            Type_voiture typeVoiture = modeleVoiturePair.left;
+                            Voiture voiture = modeleVoiturePair.right;
+
+                            System.out.println("ID: " + typeVoiture.getId_type_voiture());
+                            System.out.println("Nom: " + typeVoiture.getNom_type_voiture());
+                            System.out.println("Marque: " + typeVoiture.getMarque_voiture());
+                            System.out.println("Type: " + typeVoiture.getType());
+                            System.out.println("Description: " + typeVoiture.getDescription());
+
+                            if (voiture != null) {
+                                System.out.println("Prix par jour: " + voiture.getPrix_par_jour() + " Euros");
+                            } else {
+                                System.out.println("Prix par jour: N/A");
+                            }
+
+                            System.out.println("------------------------------");
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Prix décroissant":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    List<ConnectionUrlParser.Pair<Type_voiture, Voiture>> modeleVoiturePairs = modeleDao.searchAllModeleDESCPrice();
+
+                    if (!modeleVoiturePairs.isEmpty()) {
+                        for (ConnectionUrlParser.Pair<Type_voiture, Voiture> modeleVoiturePair : modeleVoiturePairs) {
+                            Type_voiture typeVoiture = modeleVoiturePair.left;
+                            Voiture voiture = modeleVoiturePair.right;
+
+                            System.out.println("ID: " + typeVoiture.getId_type_voiture());
+                            System.out.println("Nom: " + typeVoiture.getNom_type_voiture());
+                            System.out.println("Marque: " + typeVoiture.getMarque_voiture());
+                            System.out.println("Type: " + typeVoiture.getType());
+                            System.out.println("Description: " + typeVoiture.getDescription());
+
+                            if (voiture != null) {
+                                System.out.println("Prix par jour: " + voiture.getPrix_par_jour() + " Euros");
+                            } else {
+                                System.out.println("Prix par jour: N/A");
+                            }
+
+                            System.out.println("------------------------------");
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Berline":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String type = String.valueOf(Type_voiture.Type.BERLINE);
+                    List<Type_voiture> modeles = modeleDao.searchType(type);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "SUV":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String type = String.valueOf(Type_voiture.Type.SUV);
+                    List<Type_voiture> modeles = modeleDao.searchType(type);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Sport":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String type = String.valueOf(Type_voiture.Type.SPORT);
+                    List<Type_voiture> modeles = modeleDao.searchType(type);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Limousine":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String type = String.valueOf(Type_voiture.Type.LIMOUSINE);
+                    List<Type_voiture> modeles = modeleDao.searchType(type);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
             case "Pick-up":
+
+                try {
+
+                    Type_voitureDaoImpl modeleDao = new Type_voitureDaoImpl(connection);
+
+                    Set<String> marques = modeleDao.searchAllMarques();
+
+                    // Afficher les marques disponibles
+                    System.out.println("Marques de voiture disponibles :");
+                    for (String marque : marques) {
+                        System.out.println("- " + marque);
+                    }
+
+                    String type = String.valueOf(Type_voiture.Type.PICK_UP);
+                    List<Type_voiture> modeles = modeleDao.searchType(type);
+                    if (!modeles.isEmpty()) {
+                        System.out.println("Modèles trouvés :");
+                        for (Type_voiture modele : modeles) {
+                            System.out.println("ID : " + modele.getId_type_voiture());
+                            System.out.println("Nom : " + modele.getNom_type_voiture());
+                            System.out.println("Marque : " + modele.getMarque_voiture());
+                            // Ajoutez d'autres informations si nécessaire
+                        }
+                    } else {
+                        System.out.println("Aucun modèle trouvé pour la marque spécifiée.");
+                    }
+
+                } catch (SQLException er) {
+                    er.printStackTrace();
+                }
+
                 break;
 
         }
