733ff258a64d6bdab1096897d6c4bde82693318f
Clement <clement.herbin@edu.ece.fr>
2024-05-19 23:30:06 +0200

    test

--- src/codes/PaymentPage.java
+++ src/codes/PaymentPage.java
@@ -45,7 +45,8 @@ public class PaymentPage extends JPanel implements ActionListener {
             VoitureDaoImpl voitureDao = new VoitureDaoImpl(connection);
             ServicePaiementReservation serviceReservation = new ServicePaiementReservation(connection);
 
-            Reservation reservation = reservationDao.getReservation(this.mainJFrame.getUneVoiture().getIdResa());
+            //Reservation reservation = reservationDao.getReservation(this.mainJFrame.getUneVoiture().getIdResa());
+            Reservation reservation = reservationDao.getReservation(70);
             System.out.println("id reservation paiement" +this.mainJFrame.getUneVoiture().getIdDeLaReservation());
             Voiture voiture = voitureDao.getVoiture(this.mainJFrame.getIdVoitureAchetees());
             System.out.println("id voiture réservé :" +this.mainJFrame.getIdVoitureAchetees());
